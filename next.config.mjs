31fa2fff8bce5a360241726ec9991b1eaee8c036
Human-Centered Interactive Technologies Lab <90462458+hcitechlab@users.noreply.github.com>
2026-04-27 00:54:29 +0900

    260427

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  output: 'export',
+  reactStrictMode: true,
   images: {
-    unoptimized: true,
+    unoptimized: true, // Disable default image optimization
   },
+  output: 'export'
 };
 
 export default nextConfig;
